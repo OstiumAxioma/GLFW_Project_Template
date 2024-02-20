23d69a599d9478c2fe165475cdb0aca05cb904ca
OstiumAxioma <ostiumaxioma@outlook.com>
2024-02-19 18:55:16 -0800

    Create Function Loader

--- main.cpp
+++ main.cpp
@@ -40,11 +40,18 @@ int main() {
 	// Set the key callback
 	glfwSetKeyCallback(window, keyCallBack);
 
+// Load the OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 	// Execute window loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents(); //Receive events
 	}
+	
 	// Terminate the GLFW environment
 	glfwTerminate();
 	return 0;
